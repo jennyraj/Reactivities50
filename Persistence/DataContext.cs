598457bd3eb2fde1c9203d42d9a3b4ddf7e0109b
BEN ARULAN <barulan@meduestmail.com>
2020-09-24 21:49:59 -0400

    Activity added

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -11,9 +11,34 @@ namespace Persistence
         }
         
         public DbSet<Value> Values { get; set; }
+        public DbSet<Activity> Activities { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            builder.Entity<Activity>()
+                .Property(a => a.Id).UseIdentityColumn();
+           
+            builder.Entity<Activity>()
+                .Property(a => a.Title)
+                .HasColumnType("varchar(50)");
+
+            builder.Entity<Activity>()
+                .Property(a => a.Description)
+                .HasColumnType("varchar(50)");
+            
+            builder.Entity<Activity>()
+                .Property(a => a.Category)
+                .HasColumnType("varchar(20)");
+            
+            builder.Entity<Activity>()
+                .Property(a => a.City)
+                .HasColumnType("varchar(20)");
+            
+            builder.Entity<Activity>()
+                .Property(a => a.Venue)
+                 .HasColumnType("varchar(30)");
+    
+            
             builder.Entity<Value>()
                 .HasData(
                     new Value {Id = 1, Name = "Value 101"},
--- Persistence/Migrations/20200925014725_Initial.cs
+++ Persistence/Migrations/20200925014725_Initial.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace Persistence.Migrations
 {
@@ -6,6 +7,24 @@ namespace Persistence.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.CreateTable(
+                name: "Activities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "varchar(50)", nullable: true),
+                    Description = table.Column<string>(type: "varchar(50)", nullable: true),
+                    Category = table.Column<string>(type: "varchar(20)", nullable: true),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    City = table.Column<string>(type: "varchar(20)", nullable: true),
+                    Venue = table.Column<string>(type: "varchar(30)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Activities", x => x.Id);
+                });
+
             migrationBuilder.CreateTable(
                 name: "Values",
                 columns: table => new
@@ -37,6 +56,9 @@ namespace Persistence.Migrations
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropTable(
+                name: "Activities");
+
             migrationBuilder.DropTable(
                 name: "Values");
         }
